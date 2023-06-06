8547f9a55a293f9b1d5d2f98ceee36313b8adefd
ThaizeSantana <thaize.santana10@gmail.com>
2023-06-06 18:21:16 -0300

    atualizacao

--- app_website/urls.py
+++ app_website/urls.py
@@ -4,7 +4,7 @@ from app_website import views
 
 urlpatterns = [
     path('', views.index),
-    path('cadastro/', views.cadastro),
-    path('login/', views.login),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
     path('filmes/', views.filmes),
 ]
--- app_website/views.py
+++ app_website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+
+
+
 
 # Create your views here.
 
@@ -6,10 +14,44 @@ def index(request):
     return render(request, 'index.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse username')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return HttpResponse('Usuário cadastrado com sucesso')
+         
+
 
 def  login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":   
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
 
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request,user)
+            return render(request, 'filmes.html')
+        else:
+            return HttpResponse('Email ou Senha inválidos')
+
+
+
+
+@login_required(login_url="login/")
 def filmes(request):
-    return render(request, 'filmes.html')
+         return render(request, 'filmes.html')
+    
